2525748deea1ad530a76dcd1d8a16f6ad2e213c3
Gianluca Giacometti <52405+gianlucagiacometti@users.noreply.github.com>
2026-06-25 17:10:20 +0200

    Add files via upload

---
+++ locales/bootstrap-pickadate.es_ES.js
@@ -0,0 +1,83 @@
+/**
+ * Bootstrap Pickadate Spanish locale.
+ */
+
+"use strict";
+
+(function(window) {
+    const locale = {
+        "code": "es_ES",
+        "months": [
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre"
+        ],
+        "monthsShort": [
+            "ene",
+            "feb",
+            "mar",
+            "abr",
+            "may",
+            "jun",
+            "jul",
+            "ago",
+            "sep",
+            "oct",
+            "nov",
+            "dic"
+        ],
+        "monthShortFormat": "mmm",
+        "weekdays": [
+            "Domingo",
+            "Lunes",
+            "Martes",
+            "Miércoles",
+            "Jueves",
+            "Viernes",
+            "Sábado"
+        ],
+        "weekdaysShort": [
+            "dom",
+            "lun",
+            "mar",
+            "mié",
+            "jue",
+            "vie",
+            "sáb"
+        ],
+        "today": "Hoy",
+        "clear": "Borrar",
+        "close": "Cerrar",
+        "previousMonth": "Previous month",
+        "nextMonth": "Next month",
+        "previousYear": "Previous year",
+        "nextYear": "Next year",
+        "selectMonth": "Select a month",
+        "selectYear": "Select a year",
+        "firstDay": 1,
+        "format": "dd/mm/yyyy",
+        "longFormat": "dddd d !de mmmm !de yyyy",
+        "direction": "ltr"
+    };
+
+    window.bootstrapPickadateLocales = window.bootstrapPickadateLocales || {};
+    window.bootstrapPickadateLocales["es_ES"] = locale;
+    window.bootstrapPickadateLocales["es"] = locale;
+    if (window.bsPickadate && typeof window.bsPickadate.registerLocale === "function") {
+        window.bsPickadate.registerLocale("es_ES", locale);
+    }
+    if (window.bsPickadate && typeof window.bsPickadate.registerLocale === "function") {
+        window.bsPickadate.registerLocale("es", locale);
+    }
+})(window);
+
+// END OF FILE
