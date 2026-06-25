b5b7debb10bd31ac9a426fd20d1c0b37a8318698
Gianluca Giacometti <52405+gianlucagiacometti@users.noreply.github.com>
2026-06-25 02:35:46 +0200

    Add French locale for Bootstrap Pickadate

---
+++ locales/bootstrap-pickadate.fr.js
@@ -0,0 +1,82 @@
+/**
+ * Bootstrap Pickadate French locale.
+ */
+
+"use strict";
+
+(function(window) {
+    const locale = {
+        "code": "fr",
+        "months": [
+            "Janvier",
+            "Février",
+            "Mars",
+            "Avril",
+            "Mai",
+            "Juin",
+            "Juillet",
+            "Août",
+            "Septembre",
+            "Octobre",
+            "Novembre",
+            "Décembre"
+        ],
+        "monthsShort": [
+            "Jan",
+            "Fev",
+            "Mar",
+            "Avr",
+            "Mai",
+            "Juin",
+            "Juil",
+            "Aou",
+            "Sep",
+            "Oct",
+            "Nov",
+            "Dec"
+        ],
+        "weekdays": [
+            "Dimanche",
+            "Lundi",
+            "Mardi",
+            "Mercredi",
+            "Jeudi",
+            "Vendredi",
+            "Samedi"
+        ],
+        "weekdaysShort": [
+            "Dim",
+            "Lun",
+            "Mar",
+            "Mer",
+            "Jeu",
+            "Ven",
+            "Sam"
+        ],
+        "today": "Aujourd'hui",
+        "clear": "Effacer",
+        "close": "Fermer",
+        "previousMonth": "Previous month",
+        "nextMonth": "Next month",
+        "previousYear": "Previous year",
+        "nextYear": "Next year",
+        "selectMonth": "Select a month",
+        "selectYear": "Select a year",
+        "firstDay": 1,
+        "format": "dd/mm/yyyy",
+        "longFormat": "dd mmmm yyyy",
+        "direction": "ltr"
+    };
+
+    window.bootstrapPickadateLocales = window.bootstrapPickadateLocales || {};
+    window.bootstrapPickadateLocales["fr_FR"] = locale;
+    window.bootstrapPickadateLocales["fr"] = locale;
+    if (window.bsPickadate && typeof window.bsPickadate.registerLocale === "function") {
+        window.bsPickadate.registerLocale("fr_FR", locale);
+    }
+    if (window.bsPickadate && typeof window.bsPickadate.registerLocale === "function") {
+        window.bsPickadate.registerLocale("fr", locale);
+    }
+})(window);
+
+// END OF FILE
